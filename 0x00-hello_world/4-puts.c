eede7e79579f693003454f351fb64b8f5184a262
Kinyikamwamburi <ruthmwamburi3@gmail.com>
2022-12-08 13:04:15 -0800

    Hello Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
-#include<stdio.h> 
-int main(void)
+#include<stdio.h>
+int main() {
+	puts("\"Programming is like building a multilingual puzzle");
 
-{
-
-	    puts("\"Programming is like building a multilingual puzzle\n");
-
-	        return 0;
-
+	return 0;
 }
