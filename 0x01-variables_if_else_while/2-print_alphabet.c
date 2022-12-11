948766c151007ffb17bb624c17a9a65ce77783c5
Kinyikamwamburi <ruthmwamburi3@gmail.com>
2022-12-11 09:51:42 -0800

    Lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This is where we start
+ *    print out lower alphabets
+ *
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a' ; alph <= 'z'; alph++)
+		putchar(alph);
+	putchar('\n');
+	return (0);
+}
+
