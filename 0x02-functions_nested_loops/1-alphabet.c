d7e46810a926a92535a64ceed81f7f1d894c2cd5
Kinyikamwamburi <ruthmwamburi3@gmail.com>
2022-12-14 23:27:30 -0800

    Alphabets using prototype function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * main - chck the code using  prototype function
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+
+}
