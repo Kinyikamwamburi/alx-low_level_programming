d1925df535e80985262972ede0e500918d5360ca
Kinyikamwamburi <ruthmwamburi3@gmail.com>
2022-12-18 06:17:03 -0800

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints 0 to 9 numbers
+ *
+ * Return: numbers 
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+	if (i = 0; i < 10; i++)
+		_putchar (i + '0');
+	_putchar ('\n');
+}
