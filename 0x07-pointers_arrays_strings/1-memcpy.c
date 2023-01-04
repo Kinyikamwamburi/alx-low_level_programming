1d1e086ce529eaf0a2717f43c1aa227d8aa19cab
Kinyikamwamburi <ruthmwamburi3@gmail.com>
2023-01-04 13:11:12 -0800

    1. memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,14 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	memcpy(dest, src, strlen(src) + n);
+	unsigned int i = 0;
 
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
 	return (dest);
 }
+
+
