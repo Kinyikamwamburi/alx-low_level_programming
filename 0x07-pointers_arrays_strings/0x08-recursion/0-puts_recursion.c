6ff1d2a0aee69ac20367e5b40e77ca111861b6de
Kinyikamwamburi <ruthmwamburi3@gmail.com>
2023-01-09 10:55:27 -0800

    Corrections

---
+++ 0x07-pointers_arrays_strings/0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse.
+ * @s: prints string
+ *
+ * Return: string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	puts(s);
+
+	return (0);
+}
