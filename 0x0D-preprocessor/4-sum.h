b0f9ecc3788bf1dd6d7792217335330aac423fef
Kinyikamwamburi <ruthmwamburi3@gmail.com>
2023-01-17 12:19:28 -0800

    computes the sum of the numbers

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File: 4-sum.h
+ * @x: integer
+ * @y: integer
+ * Desc: function-like macro SUM(x, y)
+ *       that computes the sum of the numbers x and y
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif
