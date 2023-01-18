73e5e91316819f1bae2d4482f3af0050fa9929bf
Kinyikamwamburi <ruthmwamburi3@gmail.com>
2023-01-18 11:54:52 -0800

    New type poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+/**
+ * struct dog - new type
+ * @name: Element
+ * @age: Element
+ * @owner: Element
+ *
+ * Description: create new type with elements
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
